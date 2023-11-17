70888e01869535f623df8f05ff141170feca971a
Nency <nency@Nencys-MacBook-Air.local>
2023-11-17 19:38:14 +0000

    second commmit

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <Component>
+    <App />
+  </Component>,
+  document.getElementById('root')
+);
+
